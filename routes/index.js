267aaca42129c457d6d851e5c665ea83154d9f5d
Brian Lee <briancmlee23@gmail.com>
2017-07-03 21:29:53 +1000

    Implement stripe payment handling in express

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+const config = require("../config.json");
+const stripe = require("stripe")(config["keys"]["stripe"]["secret"]);
+
 router.get("/", (req, res) => {
     res.render("index");
+});
+
+router.post("/submit", (req, res) => {
+    const body = req.body;
+
+    const donationAmount = body["donation-amount"];
+    const token = body["stripeToken"];
+
+    let charge = stripe.charges.create({
+        amount: donationAmount,
+        currency: "aud",
+        description: "test charge",
+        source: token
+    }, (err, charge) => {
+
+    });
 });
 
 module.exports = router;
